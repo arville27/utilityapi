8f6afb7d43506f50af600ef1fcd69b3083545bba
Daniel Wijaya <dwjy27@gmail.com>
2022-04-05 22:19:07 +0700

    Change how the middleware declared

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+
 app.set('json spaces', 2);
 app.use(express.json());
-app.use('/api/', require('./src/middleware/logging').logging);
+app.use('/api/', require('./src/middleware/logging'));
 app.use('/api/lyrics', require('./src/routes/api/lyrics'));
 app.use('/api/anime', require('./src/routes/api/anime'));
 app.use('/api/manga', require('./src/routes/api/manga'));
--- src/middleware/logging.js
+++ src/middleware/logging.js
@@ -1,10 +1,8 @@
-module.exports = {
-    logging(req, _, next) {
-        console.log(
-            `[${new Date().toLocaleString()}] ${req.protocol.toUpperCase()} ${req.httpVersion} ${
-                req.method
-            } ${req.originalUrl}`
-        );
-        next();
-    },
+module.exports = (req, _, next) => {
+    console.log(
+        `[${new Date().toLocaleString()}] ${req.protocol.toUpperCase()} ${req.httpVersion} ${req.method} ${
+            req.originalUrl
+        }`
+    );
+    next();
 };
