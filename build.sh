15466d94ed9a341be7539e4f37edca7a961a5f3c
Daniel Wijaya <dwjy27@gmail.com>
2022-04-24 01:09:42 +0700

    Fix typo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [[ $1 == "arm64" ]]; then
     docker build -t utilityapi:arm64 -f docker/Dockerfile.arm64 .
 elif [[ $1 == "amd64" ]]; then
-    docker build -t utilityapi:amd64 -f docker/Dockerfile.arm64 .
+    docker build -t utilityapi:amd64 -f docker/Dockerfile.amd64 .
 else
     echo "Specify platform [ arm64 | amd64 ]"
 fi
