f8b2b2a7929320b38abf8f0f34ad41a6ce0e70f3
Daniel Wijaya <dwjy27@gmail.com>
2022-05-06 20:07:50 +0700

    Optimize chromium switch for scraping purposes

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -8,7 +8,45 @@ function constructContentUri(request, id) {
 module.exports = {
     constructContentUri,
     pptr: (() => {
-        const opt = { args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+        const opt = {
+            args: [
+                '--autoplay-policy=user-gesture-required',
+                '--disable-background-networking',
+                '--disable-background-timer-throttling',
+                '--disable-backgrounding-occluded-windows',
+                '--disable-breakpad',
+                '--disable-client-side-phishing-detection',
+                '--disable-component-update',
+                '--disable-default-apps',
+                '--disable-dev-shm-usage',
+                '--disable-domain-reliability',
+                '--disable-extensions',
+                '--disable-features=AudioServiceOutOfProcess',
+                '--disable-hang-monitor',
+                '--disable-ipc-flooding-protection',
+                '--disable-notifications',
+                '--disable-offer-store-unmasked-wallet-cards',
+                '--disable-popup-blocking',
+                '--disable-print-preview',
+                '--disable-prompt-on-repost',
+                '--disable-renderer-backgrounding',
+                '--disable-setuid-sandbox',
+                '--disable-speech-api',
+                '--disable-sync',
+                '--hide-scrollbars',
+                '--ignore-gpu-blacklist',
+                '--metrics-recording-only',
+                '--mute-audio',
+                '--no-default-browser-check',
+                '--no-first-run',
+                '--no-pings',
+                '--no-sandbox',
+                '--no-zygote',
+                '--password-store=basic',
+                '--use-gl=swiftshader',
+                '--use-mock-keychain',
+            ],
+        };
         switch (process.arch) {
             case 'x64':
                 return puppeteer.launch({ ...opt, executablePath: '/usr/bin/google-chrome' });
